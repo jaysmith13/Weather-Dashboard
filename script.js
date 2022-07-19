930fad6123ace3fc362048aa22b2bba54641f99c
jaysmith13 <janeasmith13@gmail.com>
2022-07-18 23:24:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var events = [];
-var currentTime = moment().hour();
-//Current date
-$('#currentDay').text(moment().format('dddd,MMMM Do YYYY'));
+class dailyweather{
+    date = new this.date();
+    icon = {
+        name:'',
+    }
+temp=0;
+humidity=0;
+constructor(temp, humidity){
+    this.temp = temp;
+    this.humidity = humidity;
 
-//local storage
-var saveEvents =function (){
-    localStorage.setItem('events', JSON.stringify(events));
+}
+setDate(openWeatherDate){
+    this.date = new Date(openWeatherDate*1000);
+    return this;
+}
+setIconName(iconName){
+    this.icon.name = iconName.slice(0, iconName.length - 1)+ this.generateIconEnding();
+    return this;
+}
+setIconDescription(IconDescription){
+    this.Icon.description = IconDescription;
+    return this;
+    }
+}
+dailyweather.prototype.generateIconEnding = function(){
+    const hour = this.date.getHours();
+    return (hour >= 0 && hour <12)? 'n':'d';
 };
-
-var loadEvents = function (){
-    events=JSON.parse(localStorage.getItem(events));
-    if (events)}
